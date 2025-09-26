2edfb01db0690ebf39e860a60bfe9f41c27a0aa2
basechase <chase7y@gmail.com>
2025-09-26 17:01:01 -0500

    working on game over screen

---
+++ Source/ObstacleCourse/FlickerLIGHT.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/LightComponentBase.h"
+#include "FlickerLIGHT.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OBSTACLECOURSE_API UFlickerLIGHT : public ULightComponentBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ObstacleCourse/MyDirectionalLightComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/DirectionalLightComponent.h"
+#include "MyDirectionalLightComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OBSTACLECOURSE_API UMyDirectionalLightComponent : public UDirectionalLightComponent
+{
+	GENERATED_BODY()
+    	
+};
